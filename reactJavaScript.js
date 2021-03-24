8bb6223b2776a45d86e85542e70257d9e8502a61
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-23 18:42:47 -0700

    Create reactJavaScript.js

---
+++ reactJavaScript.js
@@ -0,0 +1,14 @@
+function sayHello(){
+    for (var i = 0; i < 5; i++){
+        console.log(i);
+    }
+    
+    console.log(i);
+}
+
+sayHello();
+
+
+// var -> function
+// let -> block
+// const -> block (but constant)
